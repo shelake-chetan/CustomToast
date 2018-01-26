bda8af243ac0e6cbdfd78af90f6b34f890b0ba3e
shelake-chetan <shelake.chetan@gmail.com>
2018-01-26 17:58:43 +0530

    Changes

--- app/src/main/java/co/customtoast/MainActivity.java
+++ app/src/main/java/co/customtoast/MainActivity.java
@@ -1,18 +1,43 @@
 package co.customtoast;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.customtoast.R;
 import com.customtoast.chen.customtoast.CustomToast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    Context context = MainActivity.this;
+    Button mNetworkToast,mSuccessToast,mErrorToast;
+
+    private CustomToast customToast;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        CustomToast customToast = new CustomToast(this);
-        customToast.showErrorToast("Error occured");
+        customToast = new CustomToast(this); //pass context as parameter
+        customToast.setTextColor(context.getResources().getColor(R.color.colorPrimaryDark));
+        customToast.setBackground(context.getResources().getColor(R.color.white));
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId())
+        {
+            case R.id.errorToast:
+                customToast.showErrorToast("Some error occurred, please try again");
+                break;
+            case R.id.networkToast:
+                customToast.showNetworkToast();
+                break;
+            case R.id.succToast:
+                customToast.showSuccessToast("Registered successfully");
+                break;
+        }
     }
 }
--- customtoast/src/main/java/com/customtoast/chen/customtoast/CustomToast.java
+++ customtoast/src/main/java/com/customtoast/chen/customtoast/CustomToast.java
@@ -2,10 +2,12 @@ package com.customtoast.chen.customtoast;
 
 import android.app.Activity;
 import android.content.Context;
+import android.graphics.Color;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -15,28 +17,40 @@ import com.developer.chen.customtoast.R;
  * Created by chetan on 18-12-2017.
  */
 public class CustomToast {
-    Context context;
+    private Context context;
+    private int backColor;
+    private int textColor;
 
     public CustomToast(Context context) {
+        this.backColor = context.getResources().getColor(R.color.colorAccent);
+        this.textColor = context.getResources().getColor(R.color.white);
         this.context = context;
     }
 
+    public void setBackground(int backColor)
+    {
+        this.backColor = backColor;
+    }
+    public void setTextColor(int textColor)
+    {
+        this.textColor = textColor;
+    }
     public void showErrorToast(String text) {
 
         if (context != null) {
             LayoutInflater inflater = ((Activity) context).getLayoutInflater();
-
-            // Call toast.xml file for toast layout
             View toastRoot = inflater.inflate(R.layout.custom_toast, null);
+            LinearLayout linearLayout = (LinearLayout)toastRoot.findViewById(R.id.whole_layout);
+            linearLayout.setBackgroundColor(backColor);
             TextView toastText = (TextView) toastRoot.findViewById(R.id.toast_Tv);
             toastText.setText(text);
+            toastText.setTextColor(textColor);
             ImageView toastImage =(ImageView) toastRoot.findViewById(R.id.toast_Iv);
             toastImage.setImageResource(R.drawable.icn_error);
             TextView toastSuccessTv = (TextView) toastRoot.findViewById(R.id.toast_successTv);
             toastSuccessTv.setText("Error");
+            toastSuccessTv.setTextColor(textColor);
             Toast toast = new Toast(context);
-
-            // Set layout to toast
             toast.setView(toastRoot);
             toast.setGravity(Gravity.CENTER_HORIZONTAL | Gravity.CENTER_VERTICAL| Gravity.FILL_HORIZONTAL,
                     0, 0);
@@ -50,15 +64,17 @@ public class CustomToast {
 
         if (context != null) {
             LayoutInflater inflater = ((Activity) context).getLayoutInflater();
-
-            // Call toast.xml file for toast layout
             View toastRoot = inflater.inflate(R.layout.custom_toast, null);
+            LinearLayout linearLayout = (LinearLayout)toastRoot.findViewById(R.id.whole_layout);
+            linearLayout.setBackgroundColor(backColor);
             TextView toastText = (TextView) toastRoot.findViewById(R.id.toast_Tv);
             toastText.setText(text);
+            toastText.setTextColor(textColor);
             ImageView toastImage = (ImageView) toastRoot.findViewById(R.id.toast_Iv);
             toastImage.setImageResource(R.drawable.icn_success);
             TextView toastSuccessTv = (TextView) toastRoot.findViewById(R.id.toast_successTv);
             toastSuccessTv.setText("Success");
+            toastSuccessTv.setTextColor(textColor);
             Toast toast = new Toast(context);
             toast.setView(toastRoot);
             toast.setGravity(Gravity.CENTER_HORIZONTAL | Gravity.CENTER_VERTICAL | Gravity.FILL_HORIZONTAL,
@@ -74,18 +90,18 @@ public class CustomToast {
 
         if (context != null) {
             LayoutInflater inflater = ((Activity) context).getLayoutInflater();
-
-            // Call toast.xml file for toast layout
             View toastRoot = inflater.inflate(R.layout.custom_toast, null);
+            LinearLayout linearLayout = (LinearLayout)toastRoot.findViewById(R.id.whole_layout);
+            linearLayout.setBackgroundColor(backColor);
             TextView toastText = (TextView) toastRoot.findViewById(R.id.toast_Tv);
             toastText.setText("No network available");
+            toastText.setTextColor(textColor);
             ImageView toastImage = (ImageView) toastRoot.findViewById(R.id.toast_Iv);
             toastImage.setImageResource(R.drawable.icn_error);
             TextView toastSuccessTv = (TextView) toastRoot.findViewById(R.id.toast_successTv);
             toastSuccessTv.setText("Error");
+            toastSuccessTv.setTextColor(textColor);
             Toast toast = new Toast(context);
-
-            // Set layout to toast
             toast.setView(toastRoot);
             toast.setGravity(Gravity.CENTER_HORIZONTAL | Gravity.CENTER_VERTICAL| Gravity.FILL_HORIZONTAL,
                     0, 0);
